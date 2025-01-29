55a0810f2b2db15d209fcae40a5813a5215bccc7
Nishant <nishant575051@gmail.com>
2025-01-29 11:38:00 +0530

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -4,12 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from function import *
 import pygwalker as pyg
-# from pygwalker.api.streamlit import StreamlitRenderer
-import streamlit.components.v1 as components 
 
 
 # Function to display PyGWalker visualization
-def pygwalker_visualization(df
+def pygwalker_visualization(df):
     st.subheader("Interactive Visualization with PyGWalker")
     pyg_html = pyg.walk(st.session_state.df, return_html=True)
     st.components.v1.html(pyg_html, height=800, scrolling=True)
