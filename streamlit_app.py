86bef0ce0b28f015fd25a4529f5271600df3262a
Nishant <nishant575051@gmail.com>
2025-01-29 14:11:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-from ydata_profiling import ProfileReport
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -17,9 +16,9 @@ st.set_page_config(layout="wide")
 with st.sidebar:
     select = option_menu(
         menu_title="Menu",
-        options=["Home", "Data Preview", "Data Overview", "Data Cleaning", "EDA Report", "Visualization", 
+        options=["Home", "Data Preview", "Data Overview", "Data Cleaning", "Visualization", 
                  "Prediction Model", "Contact"],
-        icons=["house", "search", "images", "arrow-repeat", "file-earmark-text", "graph-up-arrow", "boxes", "person-lines-fill"],
+        icons=["house", "search", "images", "arrow-repeat", "graph-up-arrow", "boxes", "person-lines-fill"],
         menu_icon="menu-button-wide-fill",
         default_index=0,
         orientation="vertical",
@@ -49,10 +48,6 @@ if uploaded_file:
             with st.expander("Data Cleaning", expanded=True):
                 data_cleaning(df)
 
-        elif select == "EDA Report":
-            with st.expander("EDA Report", expanded=True):
-                eda_report(df)
-        
         elif select == "Visualization":
             with st.expander("Visualization", expanded=True):
                 data_visualization(df)
@@ -86,10 +81,6 @@ else:
         elif select == "Data Cleaning":
             with st.expander("Data Cleaning", expanded=True):
                 data_cleaning(df)
-
-        elif select == "EDA Report":
-            with st.expander("EDA Report", expanded=True):
-                eda_report(df)
             
         elif select == "Visualization":
             with st.expander("Visualization", expanded=True):
@@ -117,13 +108,3 @@ else:
                 prediction()
 
         st.warning("Please upload a file to proceed.")
-
-# Function to generate EDA report
-def eda_report(df):
-    st.subheader("Exploratory Data Analysis Report")
-    
-    # Generate the EDA report
-    profile = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
-    
-    # Display the report in Streamlit
-    st_profile_report(profile)
