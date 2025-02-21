c0bedf8206064036f1f384a5e1ee76d98196ce80
Nishant <nishant575051@gmail.com>
2025-02-21 10:23:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -911,7 +911,9 @@ def merge_columns(df):
             st.rerun()
 
 def parse_date(date_str):
-    for fmt in ("%d-%m-%Y", "%d.%m.%Y", "%d/%m/%Y", "%m-%d-%Y", "%m.%d.%Y", "%m/%d/%Y", "%d-%m-%y", "%d.%m.%y", "%d/%m/%y", "%m-%d-%y", "%m.%d.%y", "%m/%d/%y"):
+    for fmt in ("%Y-%m-%d", "%y-%m-%d", "%Y-%d-%m", "%d-%m-%Y", "%d.%m.%Y", "%d/%m/%Y", 
+                "%m-%d-%Y", "%m.%d.%Y", "%m/%d/%Y", "%d-%m-%y", "%d.%m.%y", "%d/%m/%y", 
+                "%m-%d-%y", "%m.%d.%y", "%m/%d/%y"):
         try:
             return pd.to_datetime(date_str, format=fmt)
         except ValueError:
