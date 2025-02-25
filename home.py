d55087c01f5c4cafc7ce8cc3dd5173613aeace83
Nishant <nishant575051@gmail.com>
2025-02-25 11:55:50 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def home():
-   
+   st.sidebar.image("data1.png")  
     st.write(
         """
-        ### Project Overview
+        ### 2N Data Analysis & Model Building Project Overview
         This application provides various tools for managing and analyzing data.
         It is designed to help users interact with datasets, clean them, perform exploratory data analysis (EDA), and visualize data. Additionally, users can work with machine learning models and predictions.
 
@@ -13,7 +13,7 @@ def home():
         2. **Data Overview**: Get an overview of the dataset, including statistics, missing values, duplicates, and column types.
         3. **Data Cleaning**: Clean the data by handling missing values, duplicates, and encoding categorical variables.
         4. **Data Visualization**: Visualize the data with various charting options, such as pair plots, bar plots, scatter plots, etc.
-        5. **Prediction Model**: Build predictive models and analyze results.
+        5. **Prediction Model**: Build predictive models and analyze results also test your builded Model.
         6. **Contact**: Find information on how to get in touch.
 
         ### How to Use:
